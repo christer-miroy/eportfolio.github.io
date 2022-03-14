51ae633b040f020f1b0ec583019bc21d11adf4e3
unknown <chrstrzm.mry@gmail.com>
2022-03-15 02:48:48 +0800

    first commit

---
+++ jsprojects.js
@@ -0,0 +1,5 @@
+let greeting = document.getElementById("greetBtn");
+greeting.addEventListener('click', () => {
+    let textGreet = "Hello World!";
+    getElementById("greet").innerHTML = textGreet;
+})
